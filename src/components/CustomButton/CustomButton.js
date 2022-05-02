aff2dd1eb402a7165a327d99f8cd34f3ac63c9b5
tanvirs2 <tanvir603@gmail.com>
2022-05-02 14:03:42 +0600

    init commit

---
+++ src/components/CustomButton/CustomButton.js
@@ -0,0 +1,33 @@
+
+import {StyleSheet, Text, View, TextInput} from 'react-native';
+
+export default function CustomInput({value, setValue, placeholder, secureTextEntry}) {
+
+    return (
+        <View style={styles.container}>
+            <TextInput
+                placeholder={placeholder}
+                value={value}
+                onChangeText={setValue}
+                style={styles.input}
+                secureTextEntry={secureTextEntry}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#ffffff",
+        width: "100%",
+
+        borderColor: "#b9a3a3",
+        borderWidth: 1,
+        borderRadius: 5,
+
+        marginVertical: 5,
+        padding: 10,
+    },
+
+    input: {}
+});
---
+++ src/screens/SignUpScreen/SignUpScreen.js
@@ -0,0 +1,62 @@
+
+import {StyleSheet, Text, View, Image, useWindowDimensions, ScrollView} from 'react-native';
+import {useState} from "react";
+import logo from "../../../assets/images/nature-logo.png"
+import CustomInput from "../../components/CustomInput"
+import CustomButton from "../../components/CustomButton";
+
+export default function SignInScreen() {
+
+    const {height} = useWindowDimensions();
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+
+    const onSignInPressed = () => {
+        console.warn('Hello')
+    }
+    const onSignInWithFacebookPressed = () => {
+        console.warn('Hello')
+    }
+    const onSignInWithGooglePressed = () => {
+        console.warn('Hello')
+    }
+    const onSignInWithApplePressed = () => {
+        console.warn('Hello')
+    }
+
+
+    const onForgotPasswordPressed = () => {
+        console.warn('onForgotPasswordPressed')
+    }
+
+    return (
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={styles.container}>
+                <Image source={logo} style={[styles.logo, {height: height * 0.3}]} resizeMode="contain" />
+
+                <CustomInput placeholder="Username" value={username} setValue={setUsername}/>
+                <CustomInput placeholder="Password" value={password} setValue={setPassword} secureTextEntry />
+                <CustomButton text="Sign in" onPress={onSignInPressed}/>
+                <CustomButton text="Forgot Password" onPress={onForgotPasswordPressed} type="tertiary"/>
+                <CustomButton text="Sign in with Facebook" onPress={onSignInWithFacebookPressed} bgColor="#B4BDFFCC" fgColor="#2846FFCC"/>
+                <CustomButton text="Sign in with Google" onPress={onSignInWithGooglePressed} bgColor="#ECD9DAFF" fgColor="#DA2C3DFF"/>
+                <CustomButton text="Sign in with Apple" onPress={onSignInWithApplePressed} bgColor="#93939347" fgColor="#202020CC"/>
+                <CustomButton text="Dont have an account? Create one" onPress={onForgotPasswordPressed} type="tertiary"/>
+            </View>
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        padding: 10,
+        color: "rgba(147,147,147,0.28)"
+    },
+    logo: {
+        height: 100,
+        width: 200,
+        maxWidth: 500,
+        maxHeight: 300,
+    },
+});
